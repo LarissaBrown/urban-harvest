c61242c86ea3094aa1270fe76825b788ca1bd96b
Larissa Brown <larisa.brown.johnson@gmail.com>
2021-04-18 08:33:31 -0600

    Delete config.js

--- functions/util/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  apiKey: "AIzaSyD32eeulivRvXbkoXPJMwm2mgwusVB7bKo",
-  authDomain: "urban-harvest-15357.firebaseapp.com",
-  projectId: "urban-harvest-15357",
-  storageBucket: "urban-harvest-15357.appspot.com",
-  messagingSenderId: "276047689214",
-  appId: "1:276047689214:web:26801f22f4595a96f37caf",
-  measurementId: "G-CJSGXHD32F",
-};
